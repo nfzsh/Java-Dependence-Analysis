303c0faa5e43c4d1f5df621701bf5b82aaff8838
李盼庚 <lipg@outlook.com>
2022-06-01 10:01:33 +0800

    初始化项目

---
+++ src/test/java/cn/lipg/instrument/jda/JdaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lipg.instrument.jda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
